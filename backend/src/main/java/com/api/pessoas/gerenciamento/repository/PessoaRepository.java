baa1507cd89b8d973438ccf1cc49b756068e9238
phdfreitas <pedro.hnd.freitas@gmail.com>
2023-02-23 17:27:12 -0300

    Spring Security, criptografia na senha e adicionando 'nível de acesso'

--- backend/src/main/java/com/api/pessoas/gerenciamento/repository/PessoaRepository.java
+++ backend/src/main/java/com/api/pessoas/gerenciamento/repository/PessoaRepository.java
@@ -2,6 +2,11 @@ package com.api.pessoas.gerenciamento.repository;
 
 import com.api.pessoas.gerenciamento.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+    @Query("select p from Pessoa p join fetch p.roles where p.email=(:email)")
+    public Pessoa findByEmail(@Param("email") String email);
 }
