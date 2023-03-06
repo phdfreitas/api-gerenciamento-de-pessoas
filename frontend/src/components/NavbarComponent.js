08cde04e336fc19b51b7d4b2033851ae3d7a6450
phdfreitas <pedro.hnd.freitas@gmail.com>
2023-03-06 20:08:47 -0300

    Barra de navagação e alterando css

--- frontend/src/components/NavbarComponent.js
+++ frontend/src/components/NavbarComponent.js
@@ -1,3 +1,5 @@
+import '../App.css'
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,14 +8,22 @@ import AuthenticationService from '../service/AuthenticationService';
 const NavbarComponent = () => {
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
+      <Navbar id='barraNavegacao'>
         <Container>
-          <Navbar.Brand href="/">Gerenciamento</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/cadastrarPessoa">Cadastrar Pessoa</Nav.Link>
-            <Nav.Link href="/listaPessoas">Lista de Pessoas</Nav.Link>
-            <Nav.Link href="/login">Entrar</Nav.Link>
-            <Nav.Link href="/" onClick={AuthenticationService.logout}>Sair</Nav.Link>
+          <Navbar.Brand href="/" id='barraNavegacao-Brand'>Gerenciamento</Navbar.Brand>
+          <Nav className="me-auto" id='barraNavegacao-Itens'>
+            {!AuthenticationService.isUserLoggedIn() &&
+              <>
+                <Nav.Link id='barraNavegacao-Itens-Item1' href="/cadastrarPessoa">Cadastrar Pessoa</Nav.Link>
+                <Nav.Link id='barraNavegacao-Itens-Item2' href="/login">Entrar</Nav.Link>
+              </>
+            }
+            {AuthenticationService.isUserLoggedIn() &&
+            <>
+              <Nav.Link id='barraNavegacao-Itens-Item3' href="/listaPessoas">Lista de Pessoas</Nav.Link>
+              <Nav.Link id='barraNavegacao-Itens-Item4' href="/" onClick={AuthenticationService.logout}>Sair</Nav.Link>
+            </>
+            }
           </Nav>
         </Container>
       </Navbar>
