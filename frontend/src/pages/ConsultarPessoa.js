3beeac15102dd4fa8edd6d4a4b5a285886ada759
phdfreitas <pedro.hnd.freitas@gmail.com>
2023-03-11 16:10:50 -0300

    Mensagem de 'nenhum endereço' (frontend)

--- frontend/src/pages/ConsultarPessoa.js
+++ frontend/src/pages/ConsultarPessoa.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom"
 import Table from 'react-bootstrap/Table';
 
 import AuthenticationService from '../service/AuthenticationService';
+import {BsExclamationOctagon} from 'react-icons/bs'
 
 const AtualizarPessoa = () => {
 
@@ -21,82 +22,98 @@ const AtualizarPessoa = () => {
     console.log(pessoa)
   return (
     <div>
-      {(currentUser.role === 'ROLE_USER' || currentUser.id === {id}) &&
+      {pessoa.length === 0 ? 
         <>
-        <h1 className="titulos-centralizados">Endereços de {pessoa.nome}</h1>
+          <div id="none-address">
 
-        <Table striped>
-          <thead>
-              <tr>
-              <th>#</th>
-              <th>Cep</th>
-              <th>Logradouro</th>
-              <th>Complemento</th>
-              <th>Bairro</th>
-              <th>UF</th>
-              <th>Cidade</th>
-              <th>Número</th>
-              <th>Tipo do Endereço</th>
-              <th>Mais informações</th>
-              </tr>
-          </thead>
-          <tbody>
-            {pessoa.enderecos && pessoa.enderecos.map((endereco) => (
-              <tr key={endereco.id}>
-                <td>{endereco.id}</td>
-                <td>{endereco.cep}</td>
-                <td>{endereco.logradouro}</td>
-                <td>{endereco.complemento}</td>
-                <td>{endereco.bairro}</td>
-                <td>{endereco.uf}</td>
-                <td>{endereco.cidade}</td>
-                <td>{endereco.numero}</td>
-                <td>{endereco.tipoEndereco[0].toUpperCase() + endereco.tipoEndereco.substring(1).toLowerCase()}</td>
-                <td>
-                  <Link to={`/`}>Atualizar Endereço</Link>
-                  <Link to={`/`}>Excluir endereço</Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+            <p id="p-none-address">
+              <span>
+                <BsExclamationOctagon size={50} color="#000"/>
+              </span>
+              Não há endereços cadastrados
+            </p>
+          </div>
         </>
-      }
-
-      {currentUser.role === 'ROLE_ADMIN' &&
+        :
         <>
-        <h1 className="titulos-centralizados">Endereços do usuário {id}</h1>
+          {(currentUser.role === 'ROLE_USER' || currentUser.id === {id}) &&
+            <>
+            <h1 className="titulos-centralizados">Endereços de {pessoa.nome}</h1>
 
-        <Table striped>
-          <thead>
-              <tr>
-              <th>#</th>
-              <th>Cep</th>
-              <th>Logradouro</th>
-              <th>Complemento</th>
-              <th>Bairro</th>
-              <th>UF</th>
-              <th>Cidade</th>
-              <th>Número</th>
-              <th>Tipo do Endereço</th>
-              </tr>
-          </thead>
-          <tbody>
-            {pessoa && pessoa.map((endereco) => (
-              <tr key={endereco.id}>
-                <td>{endereco.id}</td>
-                <td>{endereco.cep}</td>
-                <td>{endereco.logradouro}</td>
-                <td>{endereco.complemento}</td>
-                <td>{endereco.bairro}</td>
-                <td>{endereco.uf}</td>
-                <td>{endereco.cidade}</td>
-                <td>{endereco.numero}</td>
-                <td>{endereco.tipoEndereco[0].toUpperCase() + endereco.tipoEndereco.substring(1).toLowerCase()}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+            <Table striped>
+              <thead>
+                  <tr>
+                  <th>#</th>
+                  <th>Cep</th>
+                  <th>Logradouro</th>
+                  <th>Complemento</th>
+                  <th>Bairro</th>
+                  <th>UF</th>
+                  <th>Cidade</th>
+                  <th>Número</th>
+                  <th>Tipo do Endereço</th>
+                  <th>Mais informações</th>
+                  </tr>
+              </thead>
+              <tbody>
+                {pessoa.enderecos && pessoa.enderecos.map((endereco) => (
+                  <tr key={endereco.id}>
+                    <td>{endereco.id}</td>
+                    <td>{endereco.cep}</td>
+                    <td>{endereco.logradouro}</td>
+                    <td>{endereco.complemento}</td>
+                    <td>{endereco.bairro}</td>
+                    <td>{endereco.uf}</td>
+                    <td>{endereco.cidade}</td>
+                    <td>{endereco.numero}</td>
+                    <td>{endereco.tipoEndereco[0].toUpperCase() + endereco.tipoEndereco.substring(1).toLowerCase()}</td>
+                    <td>
+                      <Link to={`/`}>Atualizar Endereço</Link>
+                      <Link to={`/`}>Excluir endereço</Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+            </>
+          }
+
+          {currentUser.role === 'ROLE_ADMIN' &&
+            <>
+            <h1 className="titulos-centralizados">Endereços do usuário {id}</h1>
+
+            <Table striped>
+              <thead>
+                  <tr>
+                  <th>#</th>
+                  <th>Cep</th>
+                  <th>Logradouro</th>
+                  <th>Complemento</th>
+                  <th>Bairro</th>
+                  <th>UF</th>
+                  <th>Cidade</th>
+                  <th>Número</th>
+                  <th>Tipo do Endereço</th>
+                  </tr>
+              </thead>
+              <tbody>
+                {pessoa && pessoa.map((endereco) => (
+                  <tr key={endereco.id}>
+                    <td>{endereco.id}</td>
+                    <td>{endereco.cep}</td>
+                    <td>{endereco.logradouro}</td>
+                    <td>{endereco.complemento}</td>
+                    <td>{endereco.bairro}</td>
+                    <td>{endereco.uf}</td>
+                    <td>{endereco.cidade}</td>
+                    <td>{endereco.numero}</td>
+                    <td>{endereco.tipoEndereco[0].toUpperCase() + endereco.tipoEndereco.substring(1).toLowerCase()}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+            </>
+          }
         </>
       }
     </div>
